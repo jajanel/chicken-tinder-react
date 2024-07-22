2e12a175aad94390ce99b60769c38a06f22a1dc8
Janelle <2264502@cegeplimoilou.ca>
2024-07-22 17:59:56 -0400

    Adjusted the style of the card profile

--- src/CardProfile.jsx
+++ src/CardProfile.jsx
@@ -1,31 +1,35 @@
 import SwipeButtons from "./SwipeButtons.jsx";
 import {
-    Card, CardImg, Col, Container, Row
+    Card, CardImg, Col, Row
 } from "react-bootstrap";
+
+// import profile from "assets/temporary-profiles.json";
 
 export default function CardProfile() {
     const profilePicture = ["https://blog.eggcartonstore.com/wp-content/uploads/2021/11/Chickens-for-Blog1.png"];
 
     return (<>
-        <Container>
-            <Row>
-                <Col>
 
-                    <Card border="light">
-                        <CardImg src={profilePicture} variant="top" className="rounded-circle" alt="Profile Picture"
-                                 style={{height: 400, width: 400}}/>
-                        <Card.Body>
+
+                    <Card style={{ height: "40rem", background: "#F6F6F6"}}>
+                        <CardImg src={profilePicture} variant="top" className="px-0 py-4" alt="Profile Picture"
+                                 style={{height: 380, width: 380, margin: "auto", borderRadius: "7% / 13%"}}/>
+                        <Card.Body className=" py-0">
                             <Row>
                                 <Col>
                                     <Card.Title className="text-start"> Chickens Name </Card.Title>
                                 </Col>
                                 <Col>
-                                    <Card.Subtitle className="text-end mt-auto"> Chickens Age </Card.Subtitle>
+                                    <Card.Subtitle className="text-end mt-auto"> Chickens Race </Card.Subtitle>
                                 </Col>
                             </Row>
-                            <Card.Text>
-                                Chickens life description and other details
-                            </Card.Text>
+                            <Row>
+                                <Col>
+                                    <Card.Subtitle className="text-start py-2"> Chickens Age </Card.Subtitle>
+                                </Col>
+
+                            </Row>
+                            <p className="py-2"> Chickens life description and other details convallis vel feugiat nonumes detraxit contentiones  </p>
                         </Card.Body>
                         <Card.Body>
 
@@ -33,9 +37,7 @@ export default function CardProfile() {
                         </Card.Body>
                     </Card>
 
-                </Col>
-            </Row>
-        </Container>
+
     </>
     )
 }
