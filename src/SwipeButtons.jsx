d6e717d912a8ed0a579dac91225e093dbb6936e2
Janelle <2264502@cegeplimoilou.ca>
2024-07-22 18:01:26 -0400

    Centered the buttons inside the columns

--- src/SwipeButtons.jsx
+++ src/SwipeButtons.jsx
@@ -1,26 +1,27 @@
 import HeartButton from "./HeartButton.jsx";
+import {Button, Col, Row} from "react-bootstrap";
 
 export default function SwipeButtons(){
 return(
     <>
-        <div className="row">
-            <div className="col">
-                <button className="btn btn-light btn-xl btn-circle float-left"
-                        id="left">
+        <Row className="m-auto">
+            <Col className="d-flex align-items-center justify-content-center">
+                <Button className="btn btn-light btn-xl btn-circle "
+                        id="left" style={{width: "70%"}}>
                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"
                          fillRule="evenodd" clipRule="evenodd">
                         <path
                             d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/>
                     </svg>
-                </button>
-            </div>
-            <div className="col"></div>
-            <div className="col">
-                <button className="btn btn-light btn-xl btn-circle float-right" id="right">
+                </Button>
+            </Col>
+            <Col></Col>
+            <Col className="d-flex align-items-center justify-content-center">
+                <Button className="btn btn-light btn-xl btn-circle  d-flex align-items-center justify-content-center" id="right">
                     <HeartButton/>
-                </button>
-            </div>
-        </div>
+                </Button>
+            </Col>
+        </Row>
     </>
 )
 }
