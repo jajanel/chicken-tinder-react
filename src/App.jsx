d75133e019d40a33356a04a22c081f49c20e7233
Janelle <2264502@cegeplimoilou.ca>
2024-07-25 18:41:27 -0400

    Started to create routes to access all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import Header from "./Header.jsx";
-import Match from "./Match.jsx";
-import SwiperCard from "./SwiperCard.jsx";
+import Header from "./components/Header.jsx";
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Index from "./pages";
+import FindLove from "./pages/findlove.jsx";
+import Matches from "./pages/matches.jsx";
+import Profile from "./pages/profile.jsx";
+import Account from "./pages/account.jsx";
+import Logout from "./pages/logout.jsx";
+import Login from "./pages/login.jsx";
+
 
 export default function App() {
 
-  return (
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route path="/findlove" element={<FindLove />} />
+                <Route path="/matches" element={<Matches />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route path="/login" element={<Login />}/>
+            </Routes>
+        </Router>
+    );
 
-      <div className="App">
-          <Header/>
-          <SwiperCard/>
-          <Match/>
-      </div>
-  );
 }
 
 
