850119d64c933dd031488d1925c3b616d9dacff5
Janelle <2264502@cegeplimoilou.ca>
2024-07-26 22:53:21 -0400

    Started creating the profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,8 +1,27 @@
+import {Col, Container, FormGroup, Row} from "react-bootstrap";
+import ProfilePicture from "../components/ProfilePicture.jsx";
+
 export default function Profile() {
     return (
-        <div>
-            <h1>profile</h1>
+        <>
+            <Container>
+                <Row>
+                    <Col className="d-flex justify-content-center align-items-center bg-dark" >
+                            <Col>
+                                <ProfilePicture/>
+                            </Col>
+                            <Col>
+                                <FormGroup>
+                                    <label htmlFor="">About me...</label>
+                                    <textarea className="form-control" id=""
+                                              rows="6" cols="10">
 
-        </div>
+                                    </textarea>
+                                </FormGroup>
+                            </Col>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
