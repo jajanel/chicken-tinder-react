5d09d5f5a4268847c44bb86cbfa8501b15afba23
Janelle <2264502@cegeplimoilou.ca>
2024-07-26 22:17:04 -0400

    Updated the footer links to work with routes

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,14 +1,15 @@
  import { Container, Button } from 'react-bootstrap';
+ import {Link} from "react-router-dom";
 
  export default function FooterComponent() {
      return (
          <>
-         <div className='text-center text-white bg-secondary'>
-             <Container className='p-4 pb-0'>
-                 <section className=''>
-                     <p className='d-flex justify-content-center align-items-center'>
-                         <span className='me-3'>Register for free</span>
-                         <Button type='button' className="btn-light" rounded>
+         <div className="text-center text-white bg-secondary">
+             <Container className="p-4 pb-0">
+                 <section>
+                     <p className="d-flex justify-content-center align-items-center">
+                         <span className="me-3">Register for free</span>
+                         <Button type="button" className="btn-light link-dark" as={Link} to="/login" rounded>
                              Sign up!
                          </Button>
                      </p>
@@ -16,10 +17,10 @@
              </Container>
 
              <div className='text-center p-3'>
-                 © 2024 Copyright:
-                 <a className='text-white' href='#'>
-                     Chicken Tender
-                 </a>
+
+                 <Link as={Link} className="link-light" to="/">
+                      Chicken Tender - match with the hottest cocks and chicks
+                 </Link>
              </div>
          </div>
          </>
